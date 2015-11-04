d435b302fa861dc09ce8fe6de4d116fa232f2dd3
justmiles <j.milesmaddox@gmail.com>
2015-11-04 10:07:03 -0600

    minor updates

--- recipes/server.rb
+++ recipes/server.rb
@@ -6,8 +6,8 @@
 if node['platform_family'] == 'debian'
 
 	execute 'update' do
-		command 'apt-get update'
-		action :run
+		command 'apt-get update'		
+		action :nothing
 	end
 
 	package 'software-properties-common'
@@ -17,6 +17,10 @@ if node['platform_family'] == 'debian'
 		not_if 'grep x2go/stable /etc/apt/sources.list.d/*'
 		notifies :run, 'execute[update]', :immediately
 	end
+
+	package 'x2goserver'
+	package 'x2goserver-xsession'
+	#package 'x2gognomebindings'
 
 	#apt_repository 'x2go-stable' do
 	#  uri 'http://packages.x2go.org/debian'
--- recipes/xfce4.rb
+++ recipes/xfce4.rb
@@ -5,23 +5,26 @@
 
 if node['platform_family'] == 'debian'
 
+	execute 'update' do
+		command 'apt-get update'
+		action :nothing
+	end
 	
+	execute 'upgrade' do
+		command 'apt-get upgrade -y'
+		action :nothing
+	end
+
 	execute 'xfce-4.12' do
 		command 'add-apt-repository ppa:xubuntu-dev/xfce-4.12 -y'
 		not_if 'grep xubuntu-dev/xfce-4.12 /etc/apt/sources.list.d/*'
+		notifies :run, 'execute[update]', :immediately
 	end
 
 	execute 'extras' do
 		command 'add-apt-repository ppa:xubuntu-dev/extras -y'
 		not_if 'grep xubuntu-dev/extras /etc/apt/sources.list.d/*'
-	end
-
-	execute 'update' do
-		command 'apt-get update'
-	end
-
-	execute 'upgrade' do
-		command 'apt-get upgrade -y'
+		notifies :run, 'execute[update]', :immediately
 	end
 
 	package 'xfce4'
